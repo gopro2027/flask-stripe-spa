f3050cd05acf2e5c2d57d57aaf3e82a112e5d46c
gopro2027 <gopro2027@gmail.com>
2025-01-13 21:33:04 -0500

    Updated GUI program, removed console program in favor of stripe UI

--- StockAdminGUI.py
+++ StockAdminGUI.py
@@ -17,31 +17,54 @@ window.minsize(width=300, height=200)
 label = tk.Label(window, text="Leave blank = not run out of stock / infinite")
 label.pack()
 
+cmdlabel = tk.Label(window, text="")
+cmdlabel.pack()
+
 def submitStock(product, entry):
     newStockCount = str(entry.get())
 
-    stripe.Product.modify(product.id,metadata={keyName:newStockCount})
+    stripe.Product.modify(product.id,metadata={"stock":newStockCount})
 
     if newStockCount == '':
-        messagebox.showinfo("Information", "Set infinite stock quanity (will not go out of stock)")
+        cmdlabel.config(text="Set infinite stock quanity (will not go out of stock) of '"+product.name+"'")
     else:
-        messagebox.showinfo("Information", "Set stock quantity of '"+product.name+"' to "+str(newStockCount))
+        cmdlabel.config(text="Set stock quantity of '"+product.name+"' to "+str(newStockCount))
+
+def sumbitDisabled(product, check_var):
+    stripe.Product.modify(product.id,metadata={"disabled":("true" if check_var.get() == 1 else '')})
+    if check_var.get() == 1:
+        cmdlabel.config(text="Disabled "+product.name)
+    else:
+        cmdlabel.config(text="Enabled "+product.name)
 
 for product in products.data:
 
-    keyName = "stock"
+    frame = tk.Frame(window)
+
     stockCount = ''
-    if keyName in product.metadata:
-        stockCount = str(product.metadata[keyName])
+    if "stock" in product.metadata:
+        stockCount = str(product.metadata["stock"])
 
-    label = tk.Label(window, text=product.name+" ("+product.id+"):")
-    label.pack()
+    disabled = False
+    if "disabled" in product.metadata:
+        disabled = True
 
-    entry = tk.Entry(window)
+    label = tk.Label(frame, text=product.name+" ("+product.id+"):")
+    label.grid(column=0, row=0)
+
+    check_var = tk.IntVar(value=disabled)
+    checkbox = tk.Checkbutton(frame, text="Disabled?", variable=check_var)
+    checkbox.grid(column=0, row=1)
+    button = tk.Button(frame, text="Submit Disabled", command=lambda a = product, b = check_var: sumbitDisabled(a, b))
+    button.grid(column=1, row=1)
+
+    entry = tk.Entry(frame)
     entry.insert(tk.END, stockCount)
-    entry.pack()
+    entry.grid(column=0, row=2)
 
-    button = tk.Button(window, text="Submit", command=lambda a = product, b = entry: submitStock(a, b))
-    button.pack()
+    button = tk.Button(frame, text="Submit Stock", command=lambda a = product, b = entry: submitStock(a, b))
+    button.grid(column=1, row=2)
+
+    frame.pack()
 
 window.mainloop()
